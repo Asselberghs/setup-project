2eb46865b13ce6575f5d0f677952d8aea4859488
Nick Asselberghs <nick@asselberghs.dk>
2015-01-22 13:46:36 +0100

    Open Source GNU GPL Version 3 Added

--- Setup-step2.php
+++ Setup-step2.php
@@ -1,3 +1,20 @@
+<!-- 
+    This is a setup script for the media databases.
+    Copyright (C) 2013 Nick Tranholm Asselberghs
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
 <html><head><title>Movie Database Setup Script</title></head><body>
 <form name="setup" action="<?php echo 'Setup-step3.php';?>" method="post">
 Script User: <input type="text" name="scriptuser" value=""><br />
